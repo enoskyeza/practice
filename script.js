abf1d04dafabc878df7f58829bfae2ed35c98486
enoskyeza <enoskyeza@gmail.com>
2024-01-11 23:38:00 +0300

    Add draw func

--- script.js
+++ script.js
@@ -14,4 +14,16 @@
         context.beginPath();
       }
 
+      function draw(e) {
+        if (!isDrawing) return;
+
+        context.lineWidth = 5;
+        context.lineCap = "round";
+        context.strokeStyle = "#000";
+
+        context.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        context.stroke();
+        context.beginPath();
+        context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+      }
     });
