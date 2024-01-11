a9e7af8176a19ed36e2efd7762080f460ebf502c
enoskyeza <enoskyeza@gmail.com>
2024-01-11 23:36:42 +0300

    declare vars

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
+// Canvas feature
+    document.addEventListener("DOMContentLoaded", () => {
+      const canvas = document.getElementById("drawingCanvas");
+      const context = canvas.getContext("2d");
+      let isDrawing = false;
 
-    // JavaScript for Drag and Drop functionality
-    function allowDrop(ev) {
-      ev.preventDefault();
-    }
-
-    function drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-      }
-
-      function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        ev.target.appendChild(document.getElementById(data));
-      }
-
-
-    // JavaScript for Custom Element
-    class CustomDropdown extends HTMLElement {
-      constructor() {
-        super();
-        const template = document.getElementById('custom-dropdown').content;
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.appendChild(template.cloneNode(true));
-      }
-    }
-    customElements.define('custom-dropdown', CustomDropdown);
+      });
